46021afaac4338b0783486d09c29eaa6b3ae780f
PhemeloPearl <151036106+PhemeloPearl@users.noreply.github.com>
2026-02-27 23:34:41 +0200

    Added device type selection and brand validation

--- src/test/java/loginToNdosiWebsite.java
+++ src/test/java/loginToNdosiWebsite.java
@@ -8,15 +8,16 @@ public class loginToNdosiWebsite {
 
     WebDriver driver;
 
-
     @Test
-    public void loginWithCredentials() throws InterruptedException{
+    public void loginWithCredentials() throws InterruptedException {
 
         driver = new ChromeDriver();
         driver.get("https://ndosisimplifiedautomation.vercel.app/");
         driver.manage().window().maximize();
 
-        driver.findElement(By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[3]/button/span[2]")).click(); //clicks the login window
+        // Click the login window
+        driver.findElement(By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[3]/button/span[2]")).click();
+
         driver.findElement(By.id("login-email")).sendKeys("sandra@gmail.com");
         driver.findElement(By.id("login-password")).sendKeys("sanDRA21#");
         driver.findElement(By.id("login-submit")).click();
@@ -26,10 +27,32 @@ public class loginToNdosiWebsite {
         driver.findElement(By.xpath("//*[@id=\"app-main-content\"]/section/div[1]/h2")).isDisplayed();
 
         String myCoursesText = driver.findElement(By.xpath("//*[@id=\"app-main-content\"]/section/div[1]/h2")).getText();
+
         System.out.println(myCoursesText);
-        Assert.assertEquals(myCoursesText,"Welcome back, Montjo! \uD83D\uDC4B");
+
+        Assert.assertEquals(myCoursesText, "Welcome back, Montjo! 👋");
+
+        Thread.sleep(5000);
+
+        driver.findElement(By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[2]/div[1]/button/span[2]")).click();
+        driver.findElement(By.cssSelector("#app-root > nav > div.nav-container > div.nav-items > div:nth-child(4) > div > button:nth-child(2)")).click();
+
+        Thread.sleep(5000);
+
+        driver.findElement(By.id("practice-heading")).isDisplayed();
+
+        String practiceHeadingText = driver.findElement(By.id("practice-heading")).getText();
+        System.out.println(practiceHeadingText);
+        Assert.assertEquals(practiceHeadingText, "Welcome back, Montjo!");
+
+        Thread.sleep(5000);
+
+        driver.findElement(By.id("tab-btn-web")).click();
+        driver.findElement(By.id("deviceType")).click();
+        boolean brandDropDown = driver.findElement(By.id("deviceType")).isDisplayed();
+        Assert.assertTrue(brandDropDown);
+        driver.findElement(By.xpath("//*[@id=\"deviceType\"]/option[2]")).click();
+
 
     }
-
-
 }
