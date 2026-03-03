c4283bc9455fe73cc8a961532aa9dd72d21e7ab8
PhemeloPearl <151036106+PhemeloPearl@users.noreply.github.com>
2026-03-03 22:12:42 +0200

    Selection form

--- src/test/java/loginToNdosiWebsite.java
+++ src/test/java/loginToNdosiWebsite.java
@@ -1,6 +1,9 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -48,10 +51,57 @@ public class loginToNdosiWebsite {
         Thread.sleep(5000);
 
         driver.findElement(By.id("tab-btn-web")).click();
-        driver.findElement(By.id("deviceType")).click();
-        boolean brandDropDown = driver.findElement(By.id("deviceType")).isDisplayed();
-        Assert.assertTrue(brandDropDown);
-        driver.findElement(By.xpath("//*[@id=\"deviceType\"]/option[2]")).click();
+
+        Select deviceDropDown = new Select(driver.findElement(By.id("deviceType")));
+        deviceDropDown.selectByVisibleText("Phone");
+
+        Select brandDropDown = new Select(driver.findElement(By.id("brand")));
+        brandDropDown.selectByVisibleText("Apple");
+
+        WebElement phoneRadio = driver.findElement(By.id("storage-128GB"));
+        phoneRadio.click();
+        Assert.assertTrue(phoneRadio.isSelected());
+
+        WebElement unitPriceElement = driver.findElement(By.id("unit-price-value"));
+        String unitPriceText = unitPriceElement.getText();
+        Assert.assertEquals(unitPriceText, "R480.00");
+
+        double unitPrice = Double.parseDouble(unitPriceText.replace("R", ""));
+        Thread.sleep(3000);
+
+
+        Select colorDropDown = new Select(driver.findElement(By.id("color")));
+        colorDropDown.selectByVisibleText("Blue");
+        Assert.assertTrue(colorDropDown.getFirstSelectedOption().getText().equals("Blue"));
+
+        WebElement quantityNumber = driver.findElement(By.id("quantity"));
+
+        quantityNumber.sendKeys(Keys.CONTROL + "a");
+        quantityNumber.sendKeys(Keys.DELETE);
+        quantityNumber.sendKeys("2");
+
+        int quantity = Integer.parseInt(quantityNumber.getAttribute("value"));
+        Assert.assertEquals(quantity, 2);
+
+        double expectedSubtotal = unitPrice * quantity;
+
+        WebElement subtotalElement = driver.findElement(By.id("subtotal-value"));
+        String subtotalText = subtotalElement.getText();
+        double actualSubtotal = Double.parseDouble(subtotalElement.getText().replace("R", ""));
+
+        Assert.assertEquals(actualSubtotal, expectedSubtotal);
+
+        Thread.sleep(3000);
+
+        WebElement addressField = driver.findElement(By.id("address"));
+
+        addressField.clear();
+        addressField.sendKeys("123 Test Street");
+
+        Assert.assertEquals(addressField.getAttribute("value"), "123 Test Street");
+
+
+        driver.findElement(By.id("inventory-next-btn")).click();
 
 
     }
